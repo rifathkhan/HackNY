419e03002f2e064b8e34909a0c13bc41d5cda931
Rehman Arshad <rehman.arshad777@gmail.com>
2019-09-28 15:42:53 -0400

    got some minor links working , sign up, login

--- expirio/client/src/App.js
+++ expirio/client/src/App.js
@@ -7,18 +7,18 @@ import MedicationList from "./components/medication-list.component";
 import EditMedication from "./components/edit-medication.component";
 import AddMedication from "./components/add-medication.component";
 import CreateNewUser from "./components/create-user.component";
-
+import UserLogin from "./components/user-login.component";
 
 function App() {
   return (
     <Router> 
       <div className="container">
-        <Navbar />
         <br/>
         <Route path="/" exact component={MedicationList} />
         <Route path="/edit/:id" exact component={EditMedication} />
         <Route path="/create" exact component={AddMedication} />
         <Route path="/user" exact component={CreateNewUser} />
+        <Route path="/login" exact component={UserLogin} />
       </div>
     </Router>
   );
--- expirio/client/src/components/create-user.component.js
+++ expirio/client/src/components/create-user.component.js
@@ -42,28 +42,52 @@ export default class CreateUser extends Component {
   render() {
     return (
       <div>
-        <h3>Sign up</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group"> 
-            <label>Username: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
-                />
-              <label>Password: </label>
-              <input type="text"
-                required
-                className="form-control"
-                value={this.state.password}
-                onChange={this.onChangeUsername}
-              />
+        <div className="container-fluid">
+          <div className="row no-gutter">
+            <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+              <div className="col-md-8 col-lg-6">
+                <div className="login d-flex align-items-center py-5">
+                  <div className="container">
+                  <div className="row">
+                    <div className="col-md-9 col-lg-8 mx-auto">
+                      <h3 className="login-heading mb-4">Create an account!</h3>
+                      <form>
+                        <div className="form-label-group">
+                          <label for="inputEmail">User Name</label>
+                          <input name="username" type="email" id="inputUsername" className="form-control" placeholder="Todd Howard" required autofocus/>
+                        </div>
+
+                        <div className="form-label-group">
+                          <label for="inputEmail">Email address</label>
+                          <input name="email" type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus/>
+                        </div>
+
+                        <div className="form-label-group">
+                          <label for="inputPassword">Password</label>
+                          <input name="password" type="password" id="inputPassword" className="form-control" placeholder="Password" required/> 
+                        </div>
+
+                        <div className="form-label-group"> 
+                          <label for="inputPassword">Confirm Password</label>
+                          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/> 
+                        </div>
+
+                        <div className="form-label-group">
+                          <label for="inputPhoneNumber">Phone number</label>
+                          <input name="cellnumber" type="password" id="inputPassword" className="form-control" placeholder="Password" required/> 
+                        </div>
+
+                        <button className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Register</button>
+                        <a className="d-block text-center mt-2 small" href="/login">Already have an account?</a>
+
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-            <input type="submit" value="Create User" className="btn btn-primary" />
-          </div>
-        </form>
+        </div>
       </div>
     )
   }
