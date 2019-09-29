0b862a208cbd093af4bdd376994134b1f32fb5d1
Rifat Khan <khanr1@bxscience.edu>
2019-09-29 09:20:27 -0400

    styled abt us

--- expirio/client/src/components/about-us.component.js
+++ expirio/client/src/components/about-us.component.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import "../about.css"
 
-import Oren_Senpai from "../Oren_Senpai.jpg";
-import Rifat_Senpai from "../Rifat.PNG";
+import Oren from "../Oren_Senpai.jpg";
+import Rifat from "../Rifat.PNG";
 import Rehman from "../Rehman.PNG";
 
 export default class AboutUs extends Component {
@@ -15,23 +15,52 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-
-        <div>
-            <div className="color col-sm-6 col-md-9 col-lg-12 mx-auto">
-                <img className="rounded-circle placement" src={Oren_Senpai} width="200px" height="220px" alt="Oren Ben-Meir" />
-                <img className="rounded-circle placement" src={Rehman} width="200px" height="220px" alt="Oren Ben-Meir" />
-                <img className="rounded-circle placement" src={Rifat_Senpai} width="200px" height="220px" alt="Oren Ben-Meir" />
+      <div>
+        {/* Header */}
+        <header className="bg-primary text-center py-5 mb-4">
+          <div className="container">
+            <h1 className="font-weight-light text-white">About Us</h1>
+          </div>
+        </header>
+        {/* Page Content */}
+        <div className="container">
+          <div className="row">
+            {/* Team Member 1 */}
+            <div className="col-xl-4 col-md-6 mb-4">
+              <div className="card border-0 shadow">
+                <img src={Rifat} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                  <h5 className="card-title mb-0">Team Member</h5>
+                  <div className="card-text text-black-50">Web Developer</div>
+                </div>
+              </div>
             </div>
-            <div>
-            <p className="about-Rehman">Rehman Arshad</p>
-            <p className="about-Oren">Oren Ben-Meir</p>
-            <p className="about-Rifat">Rifat Khan</p>
-                <section>
-
-                </section>
+            {/* Team Member 2 */}
+            <div className="col-xl-4 col-md-6 mb-4">
+              <div className="card border-0 shadow">
+                <img src={Oren} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                  <h5 className="card-title mb-0">Team Member</h5>
+                  <div className="card-text text-black-50">Web Developer</div>
+                </div>
+              </div>
             </div>
+            {/* Team Member 3 */}
+            <div className="col-xl-4 col-md-6 mb-4">
+              <div className="card border-0 shadow">
+                <img src={Rehman} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                  <h5 className="card-title mb-0">Team Member</h5>
+                  <div className="card-text text-black-50">Web Developer</div>
+                </div>
+              </div>
+            </div>
+            {/* Team Member 4 */}
+          </div>
+          {/* /.row */}
         </div>
-
-    )
+        {/* /.container */}
+      </div>
+    );
   }
 }
